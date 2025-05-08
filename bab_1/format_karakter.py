e807977c75f569367a2b5cc0e9f19a6dc56ff939
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2025-05-09 00:50:36 +0700

    dua soal pertama selesai

---
+++ bab_1/format_karakter.py
@@ -0,0 +1,2 @@
+data = int (input('Masukkan sebuah bilangan: '))
+print(str(data).rjust(9, '*'))
---
+++ bab_1/penjumlahan_bilangan_real.py
@@ -0,0 +1,4 @@
+data1 = int (input('masukkan bilangan pertama: '))
+data2 = int (input('Masukkan bilangan kedua:'))
+total = data1 + data2
+print('jumlah bilangan:', total)
