f2508223ee9246b2b8f47d52aa5330f6e45dd66b
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2025-07-08 03:14:05 +0700

    menulis soal

--- logika-pemrograman-python-abdul-kadir/bab_3/diskon.py
+++ logika-pemrograman-python-abdul-kadir/bab_3/diskon.py
@@ -1,3 +1,8 @@
+"""
+Question:
+ Selama masa promosi, swalayan Megatama memberikan diskon sebesar 10%. Buatlah skrip yang meminta total pembelian
+ yang dimasukkan dari papan-ketik. Kemudian, skrop melaporkan total pembelian, besar diskon, dan total pembayaran.
+"""
 harga = float (input('masukkan harga barang: '))
 diskon = 0.1
 harga_diskon = harga * diskon
--- logika-pemrograman-python-abdul-kadir/bab_3/format_karakter.py
+++ logika-pemrograman-python-abdul-kadir/bab_3/format_karakter.py
@@ -1,2 +1,11 @@
+"""
+Question:
+Buatlah skrip yang membaca bilangan bulat dari papan-ketik. Sebagai contoh, bilangan yang dimasukkan adalah 203.
+Maka tampilannya berupa:
+*******203
+Jika bilangan yang dimasukkan 12345, tampilannya berupa:
+*****12345
+
+"""
 data = int (input('Masukkan sebuah bilangan: '))
 print(str(data).rjust(9, '*'))
